8bd693807a65ba4493053d1bcbdd575c13e571b1
silentanderson <anderson91@naver.com>
2025-11-24 16:46:52 +0900

    feat: 회원가입 뷰 컴포넌트 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
+import SignupAdditional from '@/views/auth/SignupAdditional.vue'
+import SignupComplete from '@/views/auth/SignupComplete.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/signup/additional',
+      name: 'SignupAdditional',
+      component: SignupAdditional
+    },
+    {
+      path: '/signup/complete',
+      name: 'SignupComplete',
+      component: SignupComplete
+    }
+  ],
 })
 
 export default router
